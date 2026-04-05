10e8d2cb0e2c61634028f9408545e4c6f3f30630
Michael Oberdorf <info@oberdorf-itc.de>
2026-04-05 11:04:18 +0200

    fix: correct path for MAPPING_FILE environment variable in test initialization

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,4 +8,6 @@ import sys
 __application_lib_path__ = os.path.join(os.path.dirname(os.path.dirname(__file__)), "src", "app")
 sys.path.append(__application_lib_path__)
 
-os.environ.setdefault("MAPPING_FILE", os.path.join(os.path.dirname(__file__), "src", "app", "etc", "mapping.json"))
+os.environ.setdefault(
+    "MAPPING_FILE", os.path.join(os.path.dirname(os.path.dirname(__file__)), "src", "app", "etc", "mapping.json")
+)
